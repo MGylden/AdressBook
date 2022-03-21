05d5348043c45fd7088c4ffebc12bf58fb5ca6e3
MGylden <mathias@gylden.net>
2022-03-21 21:29:12 +0100

    comments

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, app, render_template
 
 """
 Customer er blueprintet/manualen til objektet. 
-Self er objektet vi derefter laver f.eks. novo nordisk/Maersk/Lego som vi derefter kan give attributes
 """
 class Customer:
     def __init__(self, customerId, name, phone, email, adress, contactName, contactPhone, contactEmail, contactJobTitle) :
@@ -20,7 +19,7 @@ class Customer:
         self.contactJobTitle = contactJobTitle
     
     """
-    Printer attributes du har defineret tidligere, i en metode.
+    Printer attributes du har defineret tidligere oppe i en metode.
     """
     def printToString(self):
         print("Kunde ID: " + self.customerId)
@@ -33,7 +32,15 @@ class Customer:
         print("Kontakt E-Mail: " + self.contactEmail)
         print("Kontakts Stilling: " + self.contactJobTitle)
 
-
+"""
+Først definere vi funktionen getCustomers
+Herefter opretter vi en liste customerList = []
+Så kører vi try except block
+Så kører vores open function der åbner vores csv fil.
+herefter laver vi vores reader DictReader bruges i stedet for normal reader da den læser den første linje og bruger den som "header" og kategoriserer dem i key value pairs 
+Så laver vi vores for loop igennem csv filen, herefter laver vi en variabel i for loopet så vi henter vores class ind, og derefter smider vi vores class ind i en liste og appender det.
+Herefter kører vores andet for loop som looper igennem vores customerlist og printer vores loop og function (printToString) vi har defineret op over
+"""
 def getCustomers():
     customerList = []
     try:
@@ -49,7 +56,10 @@ def getCustomers():
         input()
     except Exception:
         print("House is on fire.." + Exception)
-
+"""
+her gør vi samme som ovenstående men returnere data så vi kan bruge den bearbejdede data udenfor vores funktion. 
+Vi bruger denne data til at display det via Flask
+"""
 def getCustomersFlask():
     customerList = []
     try:
@@ -93,29 +103,38 @@ def createCustomer():
         print("House is on fire.." + Exception)
 
 """
-Først definere vi metoden/funktionen updateCustomer, herefter printer vi "hvad er kundeID" 
-Derefter får vi input fra brugeren
+Først definere vi funktionen updateCustomer, herefter printer vi 
+Derefter opretter vi en variabel som vi kalder customerId, som får vi input fra brugeren
+Herefter opretter vi endnu en variabel "fields" som indeholder vores headers vi indlæser i "writer (fieldnames=fields)
+Herefter laver vi en boolean som vi kalder foundCustomer og sætter den til false da vi ikke den ikke vil kunne finde noget- den går så ind i vores for loop og tjekker om det "true", 
+ - hvis den ikke kan finde user input der matcher med data i csv filen bruger vi den igen i linje 202 og hvis foundCustomer er false printer vi, dette gør vi for at kunne melde tilbage til brugeren om det lykkedes eller ej
+Så laver vi en try except block
 Herefter åbner vi csv filen med open og indlæser den med DictReader som oversætter csv filen til en dictionary
-DictReader bruges i stedet for writer/reader da den læser den første linje og bruger den som "header" og - 
-kategoriserer dem som f.eks:  {'customerId': '42', 'name': 'Lego', 'phone': '33445566'}
-Herefter bruger vi et for loop som læser dictionary og leder efter user input derefter printer den. 
-Når customerId er fundet breaker vi loopet
+DictReader bruges i stedet for normal reader da den læser den første linje og bruger den som "header" og 
+- kategoriserer dem i key value pairs f.eks:  {'customerId': '42', 'name': 'Lego', 'phone': '33445566'}
+Herefter bruger vi DictWriter som åbner en ny midlertidig fil som vi kalder tempCustomercontactlist.csv da csv reader/writer ikke tillader os at opdatere direkte i filen 
+- da filen er læst af reader, derfor åbner vi en ny fil vi kan skrive i og derefter flytter vores temp fil til customercontactlist.csv og sletter vores temp fil 
+Herefter bruger vi et for loop som vi kalder row, den læser dictionary og leder efter user input(customerId) i csv filen, herefter printer vi næste linje og 
+- beder igen om input dette fortsætter, herefter kommer vores if statements, så if newcompanyname(userinput = blankt så skal den sætte det til det originale fra csv filen) 
+- altså hvis vi bare klikker f.eks. "Enter" at den ikke bare opdatere det til blankt.   
+hvis den så ikke kan finde det specifikke customerId fra user input bruger vi en else statement til at skrive det samme data som står i vores Customercontactlist.csv i vores temp fil.
+Herefter lukker vi filerne og flytter vores temp liste over i vores Customerlist det gør vi med modulet shutil. 
 """
 
 def updateCustomer():
     print("Hvad er kundeID?")
     customerId = input()
     fields = ["customerId", "name", "phone", "email", "adress", "contactName", "contactPhone", "contactEmail", "contactJobTitle"]
-    
+    foundCustomer = False
     
     try:
-        with open('static/csv/Customercontactlist.csv', mode="r+", newline="") as file, open('static/csv/tempCustomercontactlist.csv', mode="w+", newline="") as tempOutput:
+        with open('static/csv/Customercontactlist.csv', mode="r+", newline="") as file, open('static/csv/tempCustomercontactlist.csv', mode="w+", newline="") as tempCustomerList:
             reader = csv.DictReader(file, delimiter=';')
-            writer = csv.DictWriter(tempOutput, fieldnames=fields, delimiter=';')
+            writer = csv.DictWriter(tempCustomerList, fieldnames=fields, delimiter=';')
             writer.writeheader()
 
             for row in reader:
-                if customerId in row["customerId"]:
+                if customerId == row["customerId"]:
                     print("Found your customer!")
                     print(row)
                     print("hvad er det nye virksomhedsnavn?")
@@ -151,18 +170,37 @@ def updateCustomer():
                         row["contactEmail"] = newContactEmail
                     if newContactJobTitle:
                         row["contactJobTitle"] = newContactJobTitle
-                    
+                    foundCustomer = True
                     writer.writerow(row)
                     print(row)
                 else:
                     writer.writerow(row)
             file.close()
-            tempOutput.close()
+            tempCustomerList.close()
             shutil.move("static/csv/tempCustomercontactlist.csv", "static/csv/Customercontactlist.csv")
+        if foundCustomer == False:
+            print("Fandt ikke din kunde.")
         print("Tryk Enter for at vende tilbage til menuen.")
         input()
     except Exception:
         print("House is on fire.." + Exception)
+
+"""
+Først definere vi metoden/funktionen deleteCustomer, herefter printer vi 
+Derefter opretter vi en variabel som vi kalder customerId, som får vi input fra brugeren
+Herefter opretter vi endnu en variabel "fields" som indeholder vores headers vi indlæser i "writer (fieldnames=fields)
+Herefter laver vi en boolean som vi kalder foundCustomer og sætter den til false da vi ikke den ikke vil kunne finde noget- den går så ind i vores for loop og tjekker om det "true", 
+ - hvis den ikke kan finde user input der matcher med data i csv filen bruger vi den igen i linje 202 og hvis foundCustomer er false printer vi, dette gør vi for at kunne melde tilbage til brugeren om det lykkedes eller ej
+Herefter åbner vi csv filen med open og indlæser den med DictReader som oversætter csv filen til en dictionary
+DictReader bruges i stedet for normal reader da den læser den første linje og bruger den som "header" og 
+- kategoriserer dem i key value pairs f.eks:  {'customerId': '42', 'name': 'Lego', 'phone': '33445566'}
+Herefter bruger vi DictWriter som åbner en ny midlertidig fil som vi kalder tempCustomercontactlist.csv da csv reader/writer ikke tillader os at opdatere direkte i filen 
+ - da filen er læst af reader, derfor åbner vi en ny fil vi kan skrive i og derefter flytter vores temp fil til customercontactlist.csv og sletter vores temp fil 
+Herefter bruger vi et for loop som vi kalder row, den læser dictionary og leder efter user input i csv filen, hvis den finder customerId fortsætter den bare loopet og
+ - springer den linje over/skipper den og dermed er vores data slettet.
+hvis den så ikke kan finde det specifikke customerId fra user input bruger vi en else statement til at skrive det samme data som står i vores Customercontactlist.csv i vores temp fil.
+Herefter lukker vi filerne og flytter vores temp liste over i vores Customerlist det gør vi med modulet shutil. 
+"""
 
 def deleteCustomer():        
     print("Hvad er kundeID du vil slette?")
@@ -172,9 +210,9 @@ def deleteCustomer():
     
     
     try:
-        with open('static/csv/Customercontactlist.csv', mode="r+", newline="") as file, open('static/csv/tempCustomercontactlist.csv', mode="w+", newline="") as tempOutput:
+        with open('static/csv/Customercontactlist.csv', mode="r+", newline="") as file, open('static/csv/tempCustomercontactlist.csv', mode="w+", newline="") as tempCustomerList:
             reader = csv.DictReader(file, delimiter=';')
-            writer = csv.DictWriter(tempOutput, fieldnames=fields, delimiter=';')
+            writer = csv.DictWriter(tempCustomerList, fieldnames=fields, delimiter=';')
             writer.writeheader()
 
             for row in reader:
@@ -182,10 +220,8 @@ def deleteCustomer():
                     print("Kunden er fundet og slettet.")
                     foundCustomer = True
                     continue
-                else:
-                    writer.writerow(row)
             file.close()
-            tempOutput.close()
+            tempCustomerList.close()
             shutil.move("static/csv/tempCustomercontactlist.csv", "static/csv/Customercontactlist.csv")
         if foundCustomer == False:
             print("Fandt ikke din kunde, har ikke slettet noget.")
@@ -194,13 +230,6 @@ def deleteCustomer():
     except Exception:
         print("House is on fire.." + Exception)
 
-
-# #Definere objekter med data og smider dem i en samlet liste nederst
-# novonordisk = Customer(1, "Novo Nordisk", 9999999, "novonordisk@novonordisk.dk", "Novo nordisk vej 42, Bagsværd", "Birgitte", 22334455, "Birgitte@novonordisk.dk", "Supervisor")
-# maersk = Customer(2, "Maersk", 88888888, "maersk@maersk.dk", "Maerskvej 1, København", "Birgit", 11223344, "birgit@maersk.dk", "HR Manager")
-# lego = Customer(3, "Lego", 77777777, "lego@lego.dk", "legovej 1, Billund", "Mads", 44556677, "mads@lego.dk", "CTO")
-# customerList = (novonordisk, maersk, lego)
-choice = ""
 
 app = Flask(__name__)
 
@@ -211,6 +240,8 @@ def index():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
 
+#Her opretter vi variablen choice som vi sætter til ingenting, nedenunder bruger vi så choice.  
+choice = ""
 """
 Laver et while loop som fortsætter indtil du indtaster nummeret 5
 != betyder hvis du ikke indtaster 5 kører programmet fortsat men hvis du indtaster 5 afsluttes der.
@@ -229,7 +260,7 @@ while choice != "5":
 
   
 
-    #match matcher user input med nedenstående case, klikker jeg f.eks. 1 kører koden for "case 1" 
+    #match matcher user input med nedenstående case, klikker jeg f.eks. 1 kører metoden for "case 1" 
     match choice:
         case '1':
             getCustomers()
